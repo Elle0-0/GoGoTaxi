94ac2fa0f0f6c9a0b2467636f7fac06aa9a0aeb4
Elle0-0 <andreamichellevaz@gmail.com>
2023-12-05 20:18:15 +0000

    new icons and colors.

--- src/main/java/org/taxiapp/Aesthetics/Icons.java
+++ src/main/java/org/taxiapp/Aesthetics/Icons.java
@@ -1,25 +1,7 @@
 package org.taxiapp.Aesthetics;
 
 public class Icons {
-    String person;
-    String taxi;
-    String destination;
-
-
-    public Icons(){
-        this.person = " \uD83E\uDDCD\uD83C\uDFFB\u200D♀\uFE0F ";
-        this.taxi = " \uD83D\uDE97 ";
-        this.destination = " \uD83D\uDCCD ";
-    }
-    public String getPerson() {
-        return person;
-    }
-
-    public String getTaxi() {
-        return taxi;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
+    static String person = " \uD83E\uDDCD\uD83C\uDFFB\u200D♀\uFE0F ";
+    static String taxi = " \uD83D\uDE97 ";
+    static String destination = " \uD83D\uDCCD ";
 }
