9e402f4098f34632be0ba0e33ffe4d5cc0fa826b
Kabidoye-17 <23365412@studentmail.ul.ie>
2023-12-07 15:05:50 +0000

    added new taxi icon

--- src/main/java/org/taxiapp/Aesthetics/Icons.java
+++ src/main/java/org/taxiapp/Aesthetics/Icons.java
@@ -2,6 +2,7 @@ package org.taxiapp.Aesthetics;
 
 public class Icons {
     public static String person = " \uD83E\uDDCD\uD83C\uDFFB\u200D♀\uFE0F ";
-    public static String taxi = " \uD83D\uDE97 ";
+    public static String allcars = " \uD83D\uDE97 ";
     public static String destination = " \uD83D\uDCCD ";
+    public static String chosenTaxi = "\uD83D\uDE95";
 }
