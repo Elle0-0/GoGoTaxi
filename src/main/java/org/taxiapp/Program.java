3b7ae233d1a4936e44c5816499fb5dc585f8f678
Kabidoye-17 <23365412@studentmail.ul.ie>
2023-12-11 19:47:49 +0000

    implemented the way the taxi should run

--- src/main/java/org/taxiapp/Program.java
+++ src/main/java/org/taxiapp/Program.java
@@ -1,22 +1,28 @@
 package org.taxiapp;
 
+import org.taxiapp.Aesthetics.Icons;
+
 import java.io.FileNotFoundException;
 import java.security.NoSuchAlgorithmException;
 
 public class Program {
     Customer customer = new CustomerLocation();
     VehicleHiring vehicleHiring = new VehicleHiring();
+    Map map = new Map();
 
     public void launch() throws NoSuchAlgorithmException, FileNotFoundException {
-//        customer.signIn();
-//        customer.insertDestination();
-//        vehicleHiring.initialiseTaxis();
-//        vehicleHiring.getTaxisInRange(customer);
-//        customer.calculateTimeTaken(vehicleHiring);
-//        customer.tripExperience();
-//        customer.getRating();
+//      // Program myProgram = new Program();
+//        // myProgram.launch();
+
+        customer.signIn();
         customer.getCustomerLocation();
         customer.getCustomerDestination();
+        System.out.println(customer.destination.getX());
+        System.out.println(customer.destination.getY());
+        Taxi taxi = vehicleHiring.getATaxi(customer);
+        map.moveToTarget(taxi, customer.location.getX(), customer.location.getY(), Icons.person);
+        map.moveToTarget(taxi, customer.destination.getX(), customer.destination.getY(), Icons.destination);
+        customer.tripExperience();
     }
 
 }
