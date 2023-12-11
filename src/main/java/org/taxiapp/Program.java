7d5ed19b325234bfb99d8b8690b9775c69a03a1b
Kabidoye-17 <23365412@studentmail.ul.ie>
2023-12-11 22:04:47 +0000

    basic exception handling

--- src/main/java/org/taxiapp/Program.java
+++ src/main/java/org/taxiapp/Program.java
@@ -3,12 +3,16 @@ package org.taxiapp;
 import org.taxiapp.Aesthetics.Icons;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 public class Program {
     Customer customer = new CustomerLocation();
     VehicleHiring vehicleHiring = new VehicleHiring();
     Map map = new Map();
+
+    public Program() throws IOException {
+    }
 
     public void launch() throws NoSuchAlgorithmException, FileNotFoundException {
 //      // Program myProgram = new Program();
@@ -23,6 +27,7 @@ public class Program {
         map.moveToTarget(taxi, customer.location.getX(), customer.location.getY(), Icons.person);
         map.moveToTarget(taxi, customer.destination.getX(), customer.destination.getY(), Icons.destination);
         customer.tripExperience();
+
     }
 
 }
