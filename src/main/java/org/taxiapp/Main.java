a9caa165228da764adeb4f766506d1b63f2c4272
Kabidoye-17 <23365412@studentmail.ul.ie>
2023-12-11 19:49:06 +0000

    calls program launcher

--- src/main/java/org/taxiapp/Main.java
+++ src/main/java/org/taxiapp/Main.java
@@ -7,20 +7,9 @@ import java.security.NoSuchAlgorithmException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException {
-        // Program myProgram = new Program();
-        // myProgram.launch();
-        VehicleHiring hiring = new VehicleHiring();
-        Customer customer = new CustomerLocation();
-        Map map = new Map();
-        customer.signIn();
-        customer.getCustomerLocation();
-        customer.getCustomerDestination();
-        //System.out.println(customer.destination.getX());
-        //System.out.println(customer.destination.getY());
-        Taxi taxi = hiring.getATaxi(customer);
-        map.moveToTarget(taxi, customer.location.getX(), customer.location.getY(), Icons.person);
-        map.moveToTarget(taxi, customer.destination.getX(), customer.destination.getY(), Icons.destination);
-        // customer.tripExperience();
+        Program myProgram = new Program();
+        myProgram.launch();
+
 
 
     }
