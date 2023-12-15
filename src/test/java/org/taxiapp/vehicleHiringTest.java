0b8e3e353d469c50b8f99896b2d470329eb79d6a
Kabidoye-17 <23365412@studentmail.ul.ie>
2023-12-15 13:41:29 +0000

    off by one?

--- src/test/java/org/taxiapp/vehicleHiringTest.java
+++ src/test/java/org/taxiapp/vehicleHiringTest.java
@@ -223,7 +223,7 @@ public class vehicleHiringTest {
         System.setIn(inputStream);
 
         Taxi actualChosenTaxi = vh.getATaxi(customer);
-        Taxi expectedActualTaxi = (Taxi) vh.availableTaxisList.getChosenTaxi(testInput-1);
+        Taxi expectedActualTaxi = (Taxi) vh.availableTaxisList.getChosenTaxi(testInput);
         assertEquals(expectedActualTaxi, actualChosenTaxi);
 
     }
