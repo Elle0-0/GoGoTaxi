320c4b546824e7148b7257d6ba828b3e39bc52d4
Elle0-0 <andreamichellevaz@gmail.com>
2023-12-15 22:23:55 +0000

    Average rating implemented

--- src/main/java/org/taxiapp/Main.java
+++ src/main/java/org/taxiapp/Main.java
@@ -3,10 +3,11 @@ package org.taxiapp;
 import org.taxiapp.Aesthetics.Icons;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         Program myProgram = new Program();
         myProgram.launch();
     }
--- src/main/java/org/taxiapp/Taxi.java
+++ src/main/java/org/taxiapp/Taxi.java
@@ -12,7 +12,7 @@ public class Taxi extends User{
 
     // Attributes
    private double Rate;
-    private int Rating;
+    private double Rating;
     private Vehicle Taxi;
     private mapRegions region;
     private String locationName;
@@ -51,11 +51,8 @@ public class Taxi extends User{
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setRating(Taxi taxi, Customer customer) {}
+    public double getAverageRating(Taxi taxi) {return Rating;}
 
 
     public void assignRandomInformation(){
@@ -173,10 +170,5 @@ public class Taxi extends User{
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public static void main(String[] args) {
-        Taxi t = new Taxi();
-        t.getAllTaxis();
     }
 }
