ccf0ffd244bdf5f9280638fe64f190666b136325
Kabidoye-17 <23365412@studentmail.ul.ie>
2023-12-16 17:43:01 +0000

    added italics ansi code

--- src/main/java/org/taxiapp/Aesthetics/Colors.java
+++ src/main/java/org/taxiapp/Aesthetics/Colors.java
@@ -4,5 +4,7 @@ public class Colors {
     public static String reset = "\u001B[0m";
     public static String blue = "\u001B[94m" ;
     public static String pink = "\u001B[95m";
+    public static String italicStart = "\u001B[3m";
+    public static String italicEnd = "\u001B[0m";
 
 }
